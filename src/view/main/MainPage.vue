2e8e4eba8f9b0732533642e445ac04d6c8bfe597
ChenYu Xiao <apple.iiiyu@gmail.com>
2020-02-28 14:07:11 +1300

    修改一下tabbar

--- src/view/main/MainPage.vue
+++ src/view/main/MainPage.vue
@@ -2,104 +2,112 @@
   <div>
     <!--缓存的页面-->
     <keep-alive>
-      <router-view v-if="$route.meta.keepAlive" class="main-router-view" v-keep-scroll-position>
-      </router-view>
+      <router-view v-if="$route.meta.keepAlive" class="main-router-view" v-keep-scroll-position></router-view>
     </keep-alive>
     <!--不缓存的页面-->
-    <router-view v-if="!$route.meta.keepAlive" class="main-router-view">
-    </router-view>
+    <router-view v-if="!$route.meta.keepAlive" class="main-router-view"></router-view>
     <!--下方的TabBar-->
-    <van-tabbar v-model="tabBarSelect">
-      <!--TabBar项目-->
-      <van-tabbar-item v-for="(item, i) in tabItemList" :key="i" :to="item.to" replace>
-        <div class="tab-txt">{{$t(item.name)}}</div>
-        <img :src="getTabIconSrc(item, props.active)" slot="icon" slot-scope="props" class="icon-img"/>
-      </van-tabbar-item>
+
+    <van-tabbar v-model="active">
+      <van-tabbar-item
+        v-for="(item, i) in tabItemList"
+        :key="i"
+        :to="item.to"
+        :icon="item.icon"
+        replace
+      >{{$t(item.name)}}</van-tabbar-item>
+      <!-- <van-tabbar-item icon="home-o">Tab</van-tabbar-item>
+      <van-tabbar-item icon="chart-trending-o">Tab</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">Tab</van-tabbar-item>
+      <van-tabbar-item icon="user-o">Tab</van-tabbar-item>-->
     </van-tabbar>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "main-page",
-    watch: {
-      '$route'() {
-        this.checkTabSelection();
-      }
-    },
-    computed: {
-      // 获取图标资源
-      getTabIconSrc() {
-        return (item, active) => {
-          let img = active ? item.active : item.normal;
-          return 'static/img/main/' + img;
-        }
-      },
-    },
-    data() {
-      return {
-        tabBarSelect: 0,
-        // Tab栏数据
-        tabItemList: [
-          {
-            active: 'main_tab_3.png',
-            normal: 'main_tab_2.png',
-            name: 'tab_home',
-            to: "home"
-          },
-          {
-            active: 'main_tab_5.png',
-            normal: 'main_tab_4.png',
-            name: 'tab_products',
-            to: "products"
-          },
-          {
-            active: 'main_tab_7.png',
-            normal: 'main_tab_6.png',
-            name: 'tab_discovery',
-            to: "discover"
-          },
-          {
-            active: 'main_tab_1.png',
-            normal: 'main_tab_8.png',
-            name: 'tab_mine',
-            to: "mine"
-          },
-        ],
-      };
-    },
-    methods: {
-      // 看一下tab栏应给高亮哪一个
-      checkTabSelection() {
-        for (let i = 0; i < this.tabItemList.length; i++) {
-          let p = this.tabItemList[i];
-          if (this.$route.path.endsWith(p.to)) {
-            this.tabBarSelect = i;
-          }
-        }
-      }
-    },
-    mounted() {
+export default {
+  name: "main-page",
+  watch: {
+    $route() {
       this.checkTabSelection();
     }
+  },
+  computed: {
+    // 获取图标资源
+    // getTabIconSrc() {
+    //   return (item, active) => {
+    //     let img = active ? item.active : item.normal;
+    //     return "static/img/main/" + img;
+    //   };
+    // }
+  },
+  data() {
+    return {
+      active: 0,
+      tabBarSelect: 0,
+      // Tab栏数据
+      tabItemList: [
+        {
+          icon: "home-o",
+          name: "tab_home",
+          to: "home"
+        },
+        {
+          icon: "chart-trending-o",
+          name: "tab_products",
+          to: "products"
+        },
+        {
+          icon: "good-job-o",
+          name: "tab_discovery",
+          to: "discover"
+        },
+        {
+          icon: "user-o",
+
+          name: "tab_mine",
+          to: "mine"
+        }
+      ]
+    };
+  },
+  methods: {
+    // 看一下tab栏应给高亮哪一个
+    checkTabSelection() {
+      for (let i = 0; i < this.tabItemList.length; i++) {
+        let p = this.tabItemList[i];
+        if (this.$route.path.endsWith(p.to)) {
+          this.active = i;
+        }
+      }
+    }
+    // onChange() {
+    //   let p = this.tabItemList[this.active];
+    //   // this.$route.path.endsWith(p.to);
+    //   console.log(p.to);
+    // }
+  },
+  mounted() {
+    this.checkTabSelection();
   }
+};
 </script>
 
 <style scoped>
-  .tab-txt {
-    text-align: center;
-    margin-top: -2px;
-  }
+.tab-txt {
+  text-align: center;
+  margin-top: -2px;
+}
 
-  .main-router-view {
-    height: calc(100vh - 50px);
-    overflow-y: auto;
-    box-sizing: border-box;
-    background-color: #f8f8f8;
-  }
+.main-router-view {
+  height: calc(100vh - 50px);
+  overflow-y: auto;
+  box-sizing: border-box;
+  background-color: #f8f8f8;
+}
 
-  .icon-img {
-    margin-bottom: -1px;
-    height: 25px;
-  }
+.icon-img {
+  margin-bottom: -1px;
+  height: 25px;
+}
 </style>
